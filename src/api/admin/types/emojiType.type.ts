311f865c54f283df65140ce909337fc02439ee2c
马天梦 <2541817610@qq.com>
2023-02-07 20:12:55 +0800

    feat(views): emojiType add fields

--- src/api/admin/types/emojiType.type.ts
+++ src/api/admin/types/emojiType.type.ts
@@ -1,10 +1,14 @@
 export interface EmojiType {
   id: number
   title: string
+  width: string
+  height: string
   createdAt: string
   updatedAt: string
 }
 
 export interface EmojiTypeData {
   title: string
+  width: string
+  height: string
 }
--- src/views/admin/emoji/type/config.ts
+++ src/views/admin/emoji/type/config.ts
@@ -15,6 +15,14 @@ export const columns: TableColumns = [
     label: '名称'
   },
   {
+    key: 'width',
+    label: '宽度'
+  },
+  {
+    key: 'height',
+    label: '高度'
+  },
+  {
     key: 'createdAt',
     label: '创建日期',
     slot: true
@@ -36,6 +44,30 @@ export const dialogFields: FormFields<keyof EmojiTypeData> = [
       {
         required: true,
         message: '请输入名称',
+        trigger: 'change'
+      }
+    ]
+  },
+  {
+    key: 'width',
+    label: '宽度',
+    tag: 'ElInput',
+    validateRules: [
+      {
+        required: true,
+        message: '请输入宽度',
+        trigger: 'change'
+      }
+    ]
+  },
+  {
+    key: 'height',
+    label: '高度',
+    tag: 'ElInput',
+    validateRules: [
+      {
+        required: true,
+        message: '请输入高度',
         trigger: 'change'
       }
     ]
